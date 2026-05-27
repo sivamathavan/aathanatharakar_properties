1ab6f1ec990b7cd88b3b92fc6796d978c7424bed
Maddy@0212 <sivamathavan0212@gmail.com>
2026-05-27 20:41:16 +0530

    Fix prisma connection on Vercel by using standard PrismaClient and adding URL to schema

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,4 @@
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const connectionString = `${process.env.DATABASE_URL}`
-const pool = new Pool({
-  connectionString,
-  ssl: { rejectUnauthorized: false }
-})
-const adapter = new PrismaPg(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -15,6 +6,6 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({ adapter })
+  new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
